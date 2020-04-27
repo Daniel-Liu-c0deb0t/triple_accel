b252d5e6dcb5eb4fb1393aca7d1fe51b9395fc60
c0deb0t <daniel.liu02@gmail.com>
2020-04-26 20:05:20 -0700

    Temporarily removed benchmark

--- benches/jewel_benchmarks.rs
+++ benches/jewel_benchmarks.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion, black_box};
+/*use criterion::{criterion_group, criterion_main, Criterion, black_box};
 use rand::prelude::*;
 use triple_accel::jewel::*;
 
@@ -53,4 +53,4 @@ fn bench_jewel_fn(c: &mut Criterion) {
 }
 
 criterion_group!(bench_jewel, bench_jewel_fn);
-criterion_main!(bench_jewel);
+criterion_main!(bench_jewel);*/
