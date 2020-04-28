efeb856591386b81795cced1cffe55fc805fb927
c0deb0t <daniel.liu02@gmail.com>
2020-04-27 20:58:53 -0700

    Working on figuring out slowdown

--- benches/jewel_benchmarks.rs
+++ benches/jewel_benchmarks.rs
@@ -1,56 +1,85 @@
-/*use criterion::{criterion_group, criterion_main, Criterion, black_box};
+use criterion::{criterion_group, criterion_main, Criterion, black_box};
 use rand::prelude::*;
 use triple_accel::jewel::*;
+
+#[cfg(target_arch = "x86")]
+use core::arch::x86::*;
+
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64::*;
 
 fn bench_jewel_fn(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench_jewel_fn");
     let length = 30;
 
-    group.bench_function("create_adds_1", |b| b.iter(|| {
+    let mut a = unsafe {Avx1x32x8::repeating_max(black_box(length))};
+    let b = unsafe {Avx1x32x8::repeating_max(black_box(length))};
+
+    group.bench_function("create_adds_1", |bencher| bencher.iter(|| {
         unsafe {
-            let mut a = AvxNx32x8::repeating_max(black_box(length));
-            let b = AvxNx32x8::repeating_max(black_box(length));
-            a.adds(&b);
-            a
+            a.cmpeq_mut(&b);
         }
     }));
 
-    group.bench_function("create_adds_10", |b| b.iter(|| {
+    black_box(a);
+    black_box(b);
+
+    let mut a = unsafe {Avx1x32x8::repeating_max(black_box(length))};
+    let b = unsafe {Avx1x32x8::repeating_max(black_box(length))};
+
+    group.bench_function("create_adds_10", |bencher| bencher.iter(|| {
         unsafe {
-            let mut a = AvxNx32x8::repeating_max(black_box(length));
-            let b = AvxNx32x8::repeating_max(black_box(length));
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a.adds(&b);
-            a
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
+            a.cmpeq_mut(&b);
         }
     }));
 
-    group.bench_function("create_shift_insert", |b| b.iter(|| {
+    black_box(a);
+    black_box(b);
+
+    let mut a = unsafe {_mm256_set1_epi8(127i8)};
+    let b = unsafe {_mm256_set1_epi8(127i8)};
+
+    group.bench_function("simd_create_adds_1", |bencher| bencher.iter(|| {
         unsafe {
-            let mut a = AvxNx32x8::repeating_max(black_box(length));
-            a.shift_left_1();
-            a.insert_last_max();
-            a.clone()
+            a = _mm256_cmpeq_epi8(a, b);
         }
     }));
 
+    black_box(a);
+    black_box(b);
 
-    group.bench_function("create_vector", |b| b.iter(|| {
-        let mut a = vec![0u8; length];
-        a[0] = 255u8;
-        a.clone()
+    let mut a = unsafe {_mm256_set1_epi8(127i8)};
+    let b = unsafe {_mm256_set1_epi8(127i8)};
+
+    group.bench_function("simd_create_adds_10", |bencher| bencher.iter(|| {
+        unsafe {
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+            a = _mm256_cmpeq_epi8(a, b);
+        }
     }));
+
+    black_box(a);
+    black_box(b);
 
     group.finish();
 }
 
 criterion_group!(bench_jewel, bench_jewel_fn);
-criterion_main!(bench_jewel);*/
+criterion_main!(bench_jewel);
