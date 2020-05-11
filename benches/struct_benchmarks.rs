c5449f8fc2cc4612e5c65e3af523204b52a55f2a
c0deb0t <daniel.liu02@gmail.com>
2020-05-11 13:45:04 -0700

    Updated debug benchmark

--- benches/struct_benchmarks.rs
+++ benches/struct_benchmarks.rs
@@ -15,10 +15,18 @@ mod other {
             self.v = _mm256_adds_epi8(self.v, b.v);
         }
     }
+
+    #[target_feature(enable = "avx2")]
+    #[inline]
+    pub unsafe fn adds_mod(a: __m256i, b: __m256i) -> __m256i {
+        _mm256_adds_epi8(a, b)
+    }
 }
 
 fn bench_fn(c: &mut Criterion) {
     let mut group = c.benchmark_group("bench_fn");
+
+    // 1. directly use AVX2 intrinsics
 
     let mut a = black_box(unsafe {_mm256_set1_epi8(127i8)});
     let b = black_box(unsafe {_mm256_set1_epi8(127i8)});
@@ -41,6 +49,8 @@ fn bench_fn(c: &mut Criterion) {
     black_box(a);
     black_box(b);
 
+    // 2. wrap the AVX2 vector with a struct
+
     let mut a = black_box(unsafe {other::A{v: _mm256_set1_epi8(127i8)}});
     let b = black_box(unsafe {other::A{v: _mm256_set1_epi8(127i8)}});
 
@@ -61,6 +71,31 @@ fn bench_fn(c: &mut Criterion) {
 
     black_box(a);
     black_box(b);
+
+    // 3. call another function in another module
+
+    let mut a = black_box(unsafe {_mm256_set1_epi8(127i8)});
+    let b = black_box(unsafe {_mm256_set1_epi8(127i8)});
+
+    group.bench_function("avx_with_fn_mod", |bencher| bencher.iter(|| {
+        unsafe {
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+            a = other::adds_mod(a, b);
+        }
+    }));
+
+    black_box(a);
+    black_box(b);
+
+    // 4. call a function that is right here, not in another module
 
     #[target_feature(enable = "avx2")]
     #[inline]
@@ -88,6 +123,8 @@ fn bench_fn(c: &mut Criterion) {
 
     black_box(a);
     black_box(b);
+
+    // 5. call a function that does adds 10 times
 
     #[target_feature(enable = "avx2")]
     #[inline]
